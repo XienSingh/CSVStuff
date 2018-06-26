22e76f1c8531a9752587defaf3b48893b8f7b00f
Xien Singh <xien@ix.co.za>
2018-06-26 21:03:17 +0200

    Add project files.

---
+++ CSVReader/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isheader = true;
+            var reader = new StreamReader(File.OpenRead(@"~\Test.csv"));
+            List<string> headers = new List<string>();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                if (isheader)
+                {
+                    isheader = false;
+                    headers = values.ToList();
+                }
+                else
+                {
+                    int i = 0;
+                    for (i = 0; i < values.Length; i++)
+                    {
+                        Console.Write(string.Format("\n ##{0}## \n {1}", headers[i], values[i]));
+
+                    }
+                    Console.WriteLine("\n\t-------");
+
+                }
+            }
+        }
+    }
+}
